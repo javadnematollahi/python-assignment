2b4532adfc0e13444a6693df3fa133bf292481fc
javad <javad7189@yahoo.com>
2023-08-02 21:51:55 +0330

    edit 2

--- 2/dice.py
+++
@@ -1,7 +0,0 @@
-import random
-
-while True:
-    dice=random.randint(1,6)
-    print(f"dice number is {dice}")
-    if dice!=6:
-        break
--- 2/time_to_second.py
+++
@@ -1,6 +0,0 @@
-
-
-inputtime=input("Enter time in below format to convert to second:\nhh:mm:ss\n")
-arr_time=inputtime.split(":")
-second=int(arr_time[0])*3600+int(arr_time[1])*60+int(arr_time[2])
-print(f"{second} second.")

b6f08586da0ff945b9eba14f1e36c70b722c1396
javad <javad7189@yahoo.com>
2023-09-05 14:48:35 +0330

    Organize files in mini_project_snake_for_mlp folder

---
+++ mini_project_snake_for_MLP/train.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+
+X=pd.read_csv('dataset/input_data.csv')
+y=pd.read_csv('dataset/output_data.csv')
+
+X=X.drop(['last_up','last_down','last_right','last_left'],axis=1)
+X.shape, y.shape
+# X=X.drop(['food_up','food_down','food_left','food_right','dist_to_right_wall','dist_to_top_wall'],axis=1)
+print(len(X.columns))
+print(len(X))
+X.head()
+
+
+X_train, X_test, Y_train, Y_test=train_test_split(X,y,test_size=0.1)
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(16, activation=tf.keras.activations.relu),  #  input layer
+  tf.keras.layers.Dense(80, activation=tf.keras.activations.relu),
+  tf.keras.layers.Dense(64, activation=tf.keras.activations.relu),
+  tf.keras.layers.Dense(48, activation=tf.keras.activations.sigmoid), 
+  tf.keras.layers.Dense(4, activation='softmax')     # output layer
+])
+
+
+model.compile(optimizer=tf.keras.optimizers.AdamW(learning_rate=0.0001),
+              loss=tf.keras.losses.binary_crossentropy,
+              metrics=['accuracy'])
+
+
+
+output=model.fit(X_train, Y_train, epochs=2000)
+loss,accuracy=model.evaluate(X_test, Y_test)
+model.save("weights/MLP_FOR_Snake.h5")
+print(f'loss: {loss}, accuracy: {accuracy}')
+
+plt.plot(output.history["loss"],label='loss')
+plt.plot(output.history["accuracy"],label='accuracy')
+plt.title("train loss and accuracy")
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.legend()
+plt.show()

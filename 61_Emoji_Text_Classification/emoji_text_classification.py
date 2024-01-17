41f6f7dfb29f06db001f12ce85779c3cb7d9d99e
j_nematollahi <javad7189@yahoo.com>
2024-01-18 01:56:28 +0330

    61 Emoji text Classification

---
+++ 61_Emoji_Text_Classification/emoji_text_classification.py
@@ -0,0 +1,114 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import time
+
+
+class EmojiTextClassifier:
+    def __init__(self) -> None:
+        pass
+
+    def read_csv(self, file_path):
+        df = pd.read_csv(file_path)
+        X = np.array(df["sentence"])
+        Y = np.array(df["label"], dtype=int)
+        return X,Y
+
+    def load_dataset(self, dataset_path, word_vectors,feature_count):
+        self.X_train, self.Y_train = self.read_csv(f"{dataset_path}/train.csv")
+        self.X_test, self.Y_test = self.read_csv(f"{dataset_path}/test.csv")
+        x_train_avg = []
+        x_test_avg = []
+        for x_train in self.X_train:
+            x_train_avg.append(self.sentence_to_feature_vectors_avg(x_train, word_vectors,feature_count))
+        for x_test in self.X_test:    
+            x_test_avg.append(self.sentence_to_feature_vectors_avg(x_test, word_vectors,feature_count))
+        self.X_train = np.array(x_train_avg)
+        self.X_test = np.array(x_test_avg)
+
+        self.Y_train = tf.keras.utils.to_categorical(self.Y_train, num_classes=5)
+        self.Y_test = tf.keras.utils.to_categorical(self.Y_test, num_classes=5)
+        return self.X_train.shape, self.X_test.shape
+
+    def load_feature_vectors(self, features_path):
+        f = open(features_path, encoding="utf-8")
+
+        self.word_vectors = {}
+        for line in f:
+            line = line.strip().split()
+            word = line[0]
+            vector = np.array(line[1:], dtype=np.float64)
+            self.word_vectors[word] = vector
+        return  self.word_vectors
+    
+    def sentence_to_feature_vectors_avg(self, sentence, word_vectors, feature_count=50):
+        # try:
+            sente = sentence.lower()
+            words = sente.strip().split(" ")
+            sum_vectors = np.zeros((feature_count, ))
+            for word in words:
+                if word !="":
+                    sum_vectors += word_vectors[word]
+
+            avg_vectors = sum_vectors/len(words)
+            return avg_vectors
+        # except:
+        #     return "There is a problem with sentence"
+
+    def load_model(self, input_shape=(50, ), loss="categorical_crossentropy", model_path=None):
+        if model_path == None:
+            self.model = tf.keras.models.Sequential([
+                    tf.keras.layers.Dropout(0.2),
+                    tf.keras.layers.Dense(10, input_shape=input_shape, activation="relu"),
+                    tf.keras.layers.Dense(5,  activation="softmax")
+                    ])
+            self.model.compile(
+                optimizer=tf.keras.optimizers.Adam(),
+                loss=loss,
+                metrics=["accuracy"]
+            )
+        else:
+            self.model = tf.keras.models.load_model(model_path)
+    
+    def train(self, best_model_path="best_model", monitor_for_best_model='val_accuracy',monitor_for_early_stop='val_accuracy',epochs=200):
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(best_model_path,
+                                                        save_best_only=True,
+                                                        monitor=monitor_for_best_model)
+        stop_early = tf.keras.callbacks.EarlyStopping(monitor=monitor_for_early_stop, patience=50)
+        self.model.fit(self.X_train, self.Y_train, 
+                       epochs=epochs, 
+                       validation_data=(self.X_test, self.Y_test),
+                       callbacks=[stop_early, checkpoint]
+                       )
+
+    def test(self, best_model_path=None, test=True):
+        if best_model_path != None:
+            self.model = tf.keras.models.load_model(best_model_path)
+        if test == True:
+            self.model.evaluate(self.X_test, self.Y_test)
+        else:
+            self.model.evaluate(self.X_train, self.Y_train)
+
+    def predict(self, test_sentence, word_vectors, feature_count):
+        my_test_avg = self.sentence_to_feature_vectors_avg(test_sentence, word_vectors, feature_count)
+        my_test_avg = np.array([my_test_avg])
+        result = self.model.predict(my_test_avg)
+        y_pred = np.argmax(result)
+        emojies = ["❤️", "⚾", "🙂", "😒", "🍴" ]
+        print(emojies[y_pred])
+        return emojies[y_pred]
+
+
+if __name__=="__main__":
+    feature_count = 300
+    model_path_save = "best_model_300d"
+    etc = EmojiTextClassifier()
+    word_vectors = etc.load_feature_vectors("features\glove.6B.300d.txt")
+    etc.load_dataset("Emoji_Text_Classification", word_vectors, feature_count=feature_count)
+    etc.load_model(input_shape=(feature_count, ))
+    etc.train(best_model_path=model_path_save)
+    etc.test(model_path_save, test=False)
+    etc.test(model_path_save, test=True)
+    start = time.time() 
+    etc.predict("I love all you", word_vectors, feature_count)
+    print("Inference time: ",(time.time() - start))
---
+++ 61_Emoji_Text_Classification/readme.md
@@ -0,0 +1,52 @@
+# Emoji_text_Classification
+
+In this part we want to calssify text to five class:
+
+
+
+## Description
+
+For text classification I use feature of each word. These features are provided by standford NLP team for 6B tokens.
+
+link of word features:
+
+https://nlp.stanford.edu/data/glove.6B.zip
+
+Our dataset include some sentences about five subjects. The subject are our classes and they are about love, sport, pleasure, sad, food and I choose an emoji for each subject respectevily.  ["❤️", "⚾", "🙂", "😒", "🍴" ]
+
+link of dataset:
+
+https://drive.google.com/drive/folders/1bgbdrrIAxBeof4cwuM5H15Azehr68TwT?usp=sharing
+
+## How to install
+
+```
+pip install -r requirements.txt
+```
+
+##  How to run
+
+Run Below command in terminal to play snake:
+
+```
+python emoji_text_classification.py
+```
+
+
+# results
+
+I use just one dense layer with 5 neuron because I have 5 class.
+
+For 50d feature, loss and accuray were 0.6375,0.8214 and when I add a dropout layer before dense layer the loss and accuray were 0.7824 and 0.8214.
+
+| Feature vector dimension  | Train Loss | Train Accuracy  | Test Loss | Test Accuracy  | Inference Time |
+| ------------- | ------------- | ------------- | ------------- | ------------- | ------------- |
+| 50d  | 0.5123  | 0.8485  | 0.6375  | 0.8214  | 0.115s  |
+| 100d  | 0.3826  | 0.9394  | 0.5790  | 0.8750  | 0.126s  |
+| 200d  | 0.3844  | 0.9318  | 0.5583  | 0.8750  | 0.120s  |
+| 300d  | 0.2233  | 0.9848  | 0.5236  | 0.8750  | 0.119s  |
+
+
+
+
+
